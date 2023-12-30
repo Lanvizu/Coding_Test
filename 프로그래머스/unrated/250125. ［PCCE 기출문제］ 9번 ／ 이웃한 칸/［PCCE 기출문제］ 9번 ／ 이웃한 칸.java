3ec3b73598c4d8cbe1e78f5ced86168729e23689
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2023-12-30 15:58:25 +0900

    Update ［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
    Better Answer

--- 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.java
@@ -29,5 +29,20 @@ class Solution {
             }
         }
         return count;
+
+        // int n = board.length; 
+        // int count = 0; 
+        // int[] dh = { 0, 1, -1, 0}; int[] dw = {1,0,0,-1};
+
+        // for(int i = 0; i < 4; i++){
+        //     int h_check = h + dh[i]; 
+        //     int w_check = w + dw[i];
+        //     if(h_check >= 0 && h_check < n && w_check >= 0 && w_check < n){
+        //         if(board[h][w].equals(board[h_check][w_check])){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 }
