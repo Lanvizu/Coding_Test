933bca63273475ecc597daf98c2f8fccfc2e0758
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-02 21:27:57 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.34 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class Solution {
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        int length = data.length;
+        ArrayList<int[]> arrayList = new ArrayList<>();
+
+
+        for (int i = 0; i < length; i++) {
+            if (ext.equals("date") && data[i][1] < val_ext) {
+                arrayList.add(data[i]);
+            } else if (ext.equals("code") && data[i][0] < val_ext) {
+                arrayList.add(data[i]);
+            } else if (ext.equals("maximum") && data[i][2] < val_ext) {
+                arrayList.add(data[i]);
+            } else if (ext.equals("remain") && data[i][3] < val_ext) {
+                arrayList.add(data[i]);
+            }
+        }
+        if (sort_by.equals("remain")) {
+            arrayList.sort(new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return Integer.compare(o1[3], o2[3]);
+                }
+            });
+        }else if (sort_by.equals("code")) {
+            arrayList.sort(new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return Integer.compare(o1[0], o2[0]);
+                }
+            });
+        }else if (sort_by.equals("date")) {
+            arrayList.sort(new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return Integer.compare(o1[1], o2[1]);
+                }
+            });
+        }else if (sort_by.equals("maximum")) {
+            arrayList.sort(new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return Integer.compare(o1[2], o2[2]);
+                }
+            });
+        }
+        int[][] answer = new int[arrayList.size()][];
+        for (int i = 0; i < arrayList.size(); i++) {
+            answer[i] = arrayList.get(i);
+        }
+        return answer;
+    }
+}
