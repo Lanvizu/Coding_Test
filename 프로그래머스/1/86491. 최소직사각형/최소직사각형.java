29848d23934c18233487d8d0f43e2f77131673b0
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-06 20:39:57 +0900

    [level 1] Title: 최소직사각형, Time: 1.46 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max_a = 0;
+        int max_b = 0;
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] < sizes[i][1]) {
+                if (sizes[i][1] > max_a) {
+                    max_a = sizes[i][1];
+                }
+                if (sizes[i][0] > max_b) {
+                    max_b = sizes[i][0];
+                }
+            } else {
+                if (sizes[i][1] > max_b) {
+                    max_b = sizes[i][1];
+                }
+                if (sizes[i][0] > max_a) {
+                    max_a = sizes[i][0];
+                }
+            }
+        }
+        answer = max_a * max_b;
+        return answer;
+    }
+}
