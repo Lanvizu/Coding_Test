2db3dd289738ecdd4ebb224c2a0ddd7cd5271b3f
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-06 20:41:24 +0900

    Update 최소직사각형.java
    better answer

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -22,5 +22,9 @@ class Solution {
         }
         answer = max_a * max_b;
         return answer;
+
+        // return Arrays.stream(sizes).reduce((a, b) -> new int[]{
+        //         Math.max(Math.max(a[0], a[1]), Math.max(b[0], b[1])), Math.max(Math.min(a[0], a[1]), Math.min(b[0], b[1]))
+        // }).map(it -> it[0] * it[1]).get();
     }
 }
