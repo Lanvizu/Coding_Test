733021dd0f6180aeeb76cda56da42860b59c053a
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-09 18:43:29 +0900

    Update 신고 결과 받기.java
    better answer

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -38,5 +38,18 @@ class Solution {
             answer[i] = hashMap.get(id_list[i]);
         }
         return answer;
+
+        // List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        // HashMap<String, Integer> count = new HashMap<>();
+        // for (String s : list) {
+        //     String target = s.split(" ")[1];
+        //     count.put(target, count.getOrDefault(target, 0) + 1);
+        // }
+
+        // return Arrays.stream(id_list).map(_user -> {
+        //     final String user = _user;
+        //     List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+        //     return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+        // }).mapToInt(Long::intValue).toArray();
     }
 }
