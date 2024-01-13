68783c93fc6b5f99cbb8fe53259d1a70295cbc14
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-13 12:35:28 +0900

    Update 큰 수 만들기.java
    better answer

--- 프로그래머스/Lv.2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/Lv.2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -39,5 +39,18 @@ class Solution {
         }
         answer = builder.toString();
         return answer;
+
+        // StringBuilder builder = new StringBuilder(number);
+        // int length = builder.length() - k;
+
+        // for (int i = 1; i < builder.length() && builder.length() > length; i++) {
+        //     if (builder.charAt(i) > builder.charAt(i - 1)) {
+        //         builder.deleteCharAt(i - 1);
+        //         i = Math.max(0, i - 2);
+        //     }
+        // }
+
+        // return builder.substring(0, length);
+        
     }
 }
