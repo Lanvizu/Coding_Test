f7055c3ad7d74cd0631364d9869775684985a387
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-14 14:30:19 +0900

    [level Lv.2] Title: 구명보트, Time: 35.60 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.util.stream.IntStream;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int length = people.length;
+        Integer[] array = IntStream.of(people)
+                .boxed()
+                .toArray(Integer[]::new);
+        Arrays.sort(array, Collections.reverseOrder());
+        int back = 0;
+        for (int i = 0; i < length - back; i++) {
+            int count = limit - array[i];
+            if (count >= 40 && array[length - 1 - back] <= count) {
+                array[length - 1 - back] = 0;
+                back++;
+            }
+            array[i] = 0;
+            answer++;
+        }
+        return answer;
+    }
+}
