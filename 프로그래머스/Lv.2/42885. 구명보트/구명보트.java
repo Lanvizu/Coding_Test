abf81ed30ece90350dbd40b54f4de1718625f6ad
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-14 14:32:12 +0900

    Update 구명보트.java
    better answer

--- 프로그래머스/Lv.2/42885. 구명보트/구명보트.java
+++ 프로그래머스/Lv.2/42885. 구명보트/구명보트.java
@@ -19,5 +19,13 @@ class Solution {
             answer++;
         }
         return answer;
+
+        //  Arrays.sort(people);
+        // int i = 0, j = people.length - 1;
+        // for (; i < j; --j) {
+        //     if (people[i] + people[j] <= limit)
+        //         ++i;
+        // }
+        // return people.length - i;
     }
 }
