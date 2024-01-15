cbeaf1b11e001af4837a6f78263faecce6a70c93
Lanvizu <121706341+Lanvizu@users.noreply.github.com>
2024-01-15 19:24:35 +0900

    [level Lv.2] Title: 타겟 넘버, Time: 0.51 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,20 @@
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+
+    private void dfs(int sum, int x, int[] numbers, int target) {
+        if (x == numbers.length && sum == target) {
+            answer++;
+            return;
+        }
+
+        if (x >= numbers.length) {
+            return;
+        }
+        dfs(sum + numbers[x], x + 1, numbers, target);
+        dfs(sum - numbers[x], x + 1, numbers, target);
+    }
+}
