efc79d58fc4eeeba08372001a05d113622a0c72e
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2024-12-03 09:48:20 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -0,0 +1,12 @@
+import sys
+nums = sys.stdin.readline().rstrip()
+n = 0
+idx = 0
+while True:
+    n += 1
+    for s in str(n):
+        if nums[idx] == s:
+            idx += 1
+            if idx >= len(nums):
+                print(n)
+                exit()
