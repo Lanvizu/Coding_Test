a8105b33890647823c1684f5c929882ad74e0bb1
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-02-07 14:35:17 +0900

    [level 2] Title: 주식가격, Time: 71.92 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,57 @@
+import java.io.*;
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-
         Queue<Integer> q = new LinkedList<>();
-
-        for (int i : prices) {
+        for(int i : prices){
             q.add(i);
         }
-
-        int count = 0;
-        while (q.peek() != null) {
-            int poll = q.poll();
-            answer[count] = 0;
-
-            for (Integer i : q) {
-                answer[count]++;
-                if (i < poll) {
+        int cnt = 0;
+        while(q.peek() != null){
+            int tar = q.poll();
+            int val = 0;
+            for(int i : q){
+                if(i < tar){
+                    val += 1;
                     break;
                 }
+                val += 1;
             }
-            count++;
+            answer[cnt] = val;
+            cnt ++;
         }
         return answer;
     }
 }
+
+
+
+// import java.util.*;
+// class Solution {
+//     public int[] solution(int[] prices) {
+//         int[] answer = new int[prices.length];
+
+//         Queue<Integer> q = new LinkedList<>();
+
+//         for (int i : prices) {
+//             q.add(i);
+//         }
+
+//         int count = 0;
+//         while (q.peek() != null) {
+//             int poll = q.poll();
+//             answer[count] = 0;
+
+//             for (Integer i : q) {
+//                 answer[count]++;
+//                 if (i < poll) {
+//                     break;
+//                 }
+//             }
+//             count++;
+//         }
+//         return answer;
+//     }
+// }
