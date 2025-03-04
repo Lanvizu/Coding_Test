a543470653553c0bbaf3131741f38d9a8cbfbf5b
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-03-04 15:57:04 +0900

    [level 2] Title: 양궁대회, Time: 547.59 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,18 +1,33 @@
-def solution(n, info_A):
-    info_L = [0]*11
-    arrow_L = [i+1 for i in info_A[:-1]]
-    graph = [[10-index,(2-(hit==1))*(10-index),hit] for index,hit in enumerate(arrow_L)]
-    graph.sort(key=lambda x:x[0])
-    print(graph)
-    dp = [[0,[],11] for _ in range(n+1)]
-    for a,b,c in graph:
-        for x in range(n,c-1,-1):
-            if dp[x-c][0]+b > dp[x][0] or (dp[x-c][0]+b == dp[x][0] and dp[x-c][2] < dp[x][2]):
-                dp[x][0] = dp[x-c][0]+b
-                dp[x][1] = dp[x-c][1]+[a]
-                dp[x][2] = min(dp[x][1])
-    for i in dp[n][1]:
-        info_L[10-i] = arrow_L[10-i]
-    info_L[-1] = n-sum(info_L)        
-    answer = sum(((l>a)-(a>l))*(10-index) for index,(a,l) in enumerate(zip(info_A,info_L)))
-    return info_L if answer>0 else [-1]  
+def solution(n, info):
+    global answer, result
+
+    def score(ryan):
+        s = 0
+        for i in range(11):
+            if ryan[i] == info[i] == 0:
+                continue
+            if ryan[i] > info[i]:
+                s += 10 - i
+            else:
+                s -= 10 - i
+        return s
+
+    def dfs(idx, left, ryan):
+        global answer, result
+        if idx == -1 and left:
+            return
+        if left == 0:
+            s = score(ryan)
+            if result < s:
+                answer = ryan[:]
+                result = s
+            return
+        for i in range(left, -1, -1):
+            ryan[idx] = i
+            dfs(idx-1, left-i, ryan)
+            ryan[idx] = 0
+
+    answer = [0 for _ in range(11)]
+    result = 0
+    dfs(10, n, [0 for _ in range(11)])
+    return answer if result != 0 else [-1]
