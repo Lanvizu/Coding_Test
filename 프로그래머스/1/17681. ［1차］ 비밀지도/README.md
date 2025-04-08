d27120974b030c30cd126335ca2db50fbfc03848
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-04-08 10:12:54 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.49 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681#) 
 
 ### 성능 요약
 
-메모리: 88 MB, 시간: 3.13 ms
+메모리: 79.3 MB, 시간: 2.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 10:10:56
+2025년 04월 08일 10:12:48
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -4,19 +4,14 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for(int i=0; i<n; i++){
-            String a = Integer.toBinaryString(arr1[i]);
-            String b = Integer.toBinaryString(arr2[i]);
+            String a = Integer.toBinaryString(arr1[i] | arr2[i]);
             while(a.length() < n){
                 a = "0" + a;
             }
-            while(b.length() < n){
-                b = "0" + b;
-            }
             StringBuilder sb = new StringBuilder();
             String[] aa = a.split("");
-            String[] bb = b.split("");
             for(int j=0; j<n; j++){
-                if(aa[j].equals("1") || bb[j].equals("1")){
+                if(aa[j].equals("1")){
                     sb.append("#");
                 }else{
                     sb.append(" ");
