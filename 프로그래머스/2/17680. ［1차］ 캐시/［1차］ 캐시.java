91754a5d9928735c009d37318f3d0e672e24b349
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-04-08 10:33:38 +0900

    [level 2] Title: [1차] 캐시, Time: 34.50 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,27 +1,23 @@
 import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
         if (cacheSize == 0){
-            answer = cities.length * 5;
-        }else{
-            Queue<String> q = new LinkedList<>();
-            for(String city:cities){
-                city = city.toLowerCase();
-                if(q.contains(city)){
-                    q.remove(city);
-                    q.offer(city);
-                    answer += 1;
-                }else{
-                    if(q.size() >= cacheSize){
-                        q.poll();
-                    }
-                    q.offer(city);
-                    answer += 5;
-                }
-                if(q.size()>cacheSize){
+            return cities.length * 5;
+        }
+        int answer = 0;
+        Queue<String> q = new LinkedList<>();
+        for(String city:cities){
+            city = city.toLowerCase();
+            if(q.contains(city)){
+                q.remove(city);
+                q.offer(city);
+                answer += 1;
+            }else{
+                if(q.size() >= cacheSize){
                     q.poll();
                 }
+                q.offer(city);
+                answer += 5;
             }
         }
         return answer;
