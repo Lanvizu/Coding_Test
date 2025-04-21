22cffa55de4cd26e52c8b331df3dabae5d0b3b13
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-04-21 17:24:06 +0900

    [level 2] Title: [3차] 방금그곡, Time: 18.87 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -2,66 +2,45 @@ import java.util.*;
 
 class Solution {
     public String solution(String m, String[] musicinfos) {
-        HashMap<Integer, String> hs = new HashMap<>();
+        String answer = "(None)";
+        m = change(m);
+        int time = 0;
         for(String a:musicinfos){
             String[] infos = a.split(",");
-            char[] b = infos[3].toCharArray();
-            int ml = b.length;
-            List<String> music = new ArrayList<>();
-            for(int i=0; i<ml-1;){
-                if(b[i+1] != '#'){
-                    music.add(""+b[i]);
-                    i++;
-                }else{
-                    music.add(""+b[i]+b[i+1]);
-                    i+=2;
-                }
-            }
-            if(b[ml-1] != '#'){
-                music.add(""+b[ml-1]);
-            }
             
-            String[] N1 = infos[0].split(":");
-            String[] N2 = infos[1].split(":");
-            int[] n1 = new int[2];
-            int[] n2 = new int[2];
-            for(int i=0; i<2; i++){
-                n1[i] = Integer.parseInt(N1[i]);
-                n2[i] = Integer.parseInt(N2[i]);
-            }
-            int time = n2[1] - n1[1];
-            if(time < 0){
-                time += (n2[0] - n1[0] - 1)*60 +60;
-            }else{
-                time += (n2[0] - n1[0]) *60;
-            }
+            int start = (Integer.parseInt(infos[0].substring(0,2)) * 60)
+                + Integer.parseInt(infos[0].substring(3));
+                
+            int end = (Integer.parseInt(infos[1].substring(0,2)) * 60)
+                + Integer.parseInt(infos[1].substring(3));
             
-            String result = "";
-            for(int i=0; i<time; i++){
-                result += music.get(i % music.size());
-            }
-            for(int i=0; i<result.length()-m.length()+1; i++){
-                // 마지막 다음 글자가 #인 경우는 제외해야함
-                if(i<result.length()-m.length()
-                   &&result.charAt(i+m.length()) == '#'){
-                    continue;
+            int t = end - start;
+            
+            if(t > time){
+                String info = change(infos[3]);
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < t; i++) {
+                    sb.append(info.charAt(i % (info.length())));
                 }
-                String target = result.substring(i,i+m.length());
-                if(m.equals(target) && !hs.containsKey(time)){
-                    hs.put(time, infos[2]);
-                    break;
+                if (sb.toString().indexOf(m) >= 0) {
+                    answer = infos[2];
+                    time = t;
                 }
             }
         }
-        if(hs.isEmpty()){
-            return "(None)";
-        }
-        Set<Integer> hsKeys = hs.keySet();
-        List<Integer> hsKeys_sort = new ArrayList<>(hsKeys);
-        Collections.sort(hsKeys_sort);
-        return hs.get(hsKeys_sort.get(hsKeys_sort.size()-1));
+        
+        return answer;
+    }
+    
+    public String change(String m) {
+
+        m = m.replaceAll("C#", "U");
+        m = m.replaceAll("D#", "V");
+        m = m.replaceAll("F#", "W");
+        m = m.replaceAll("G#", "X");
+        m = m.replaceAll("A#", "Y");
+        m = m.replaceAll("B#", "Z");
+
+        return m;
     }
 }
-// 어떻게 접근해야할까
-// 시간별로 반복하여 일단 작성 -> CDEFGABCDEFGAB -> ABCDEFG(m)이 존재하는지 체크?
-// 저장해놓고 마지막에 일치하는 곡 끼리 비교해야함 (라디오의 시간도 같이 저장)
