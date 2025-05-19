89dba220c475fb71b3240cfffbc62e927012afda
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-05-19 18:09:01 +0900

    [level 2] Title: 비밀 코드 해독, Time: 12.38 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,44 +1,34 @@
-import java.util.*;
 class Solution {
-    int total = 0;
-    int m;
+    int answer = 0;
     int[][] Q;
     int[] A;
     public int solution(int n, int[][] q, int[] ans) {
-        m = q.length;
+        boolean[] visited = new boolean[n+1];
         Q = q;
         A = ans;
-        int[] result = new int[n+1];
-        for(int i=1; i<n+1; i++){
-            result[i] = i;
-        }
-        boolean[] visited = new boolean[n+1];
-        comb(n, visited, 1, 5);
-        return total;
+        dfs(1,5,visited);
+        return answer;
     }
-    
-    public void comb(int n, boolean[] visited, int start, int r){
+    public void dfs(int start, int r, boolean[] visited){
         if(r==0){
             calc(visited);
             return;
         }
-        for(int i=start; i<=n; i++){
+        for(int i=start; i<visited.length; i++){
             visited[i] = true;
-            comb(n,visited,i+1,r-1);
+            dfs(i+1, r-1, visited);
             visited[i] = false;
         }
     }
-    
     public void calc(boolean[] visited){
-        for(int i=0; i<m; i++){
-            int ans = A[i];
+        for(int i=0; i<Q.length; i++){
             int cnt = 0;
-            for(int qq:Q[i]){
-                if(visited[qq]) cnt++;
-                if(cnt > ans) return;
+            for(int q:Q[i]){
+                if(visited[q]) cnt++;
+                if(A[i]<cnt) return;
             }
-            if(cnt != ans) return;
+            if(A[i]!=cnt) return;
         }
-        total++;
+        answer++;
     }
 }
