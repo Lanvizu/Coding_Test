a0894f204239d21ecca2d6a47da6f9724646ca00
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-06-19 14:18:13 +0900

    [Gold II] Title: 성냥개비, Time: 160 ms, Memory: 16576 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.java
+++ 백준/Gold/3687. 성냥개비/성냥개비.java
@@ -5,73 +5,35 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    static long[] arr = new long[101];
+    static String[] dp = new String[101];
+    static String MAX_STR = "1".repeat(51);
+    static int[] arr = new int[]{6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+
+    static String compareString(String a, String b) {
+        if (a.length() == b.length()) {
+            if (a.compareTo(b) < 0) return a;
+            else return b;
+        }
+        if (a.length() < b.length()) return a;
+        else return b;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        initArr();
-        for (int i = 8; i <= 100; i++) {
-            dp(i);
-        }
-        arr[6] = 6;
         int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < 101; i++) dp[i] = MAX_STR;
+        dp[0] = "";
+        for (int i = 2; i < 101; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i < arr[j]) continue;
+                if (j == 0 && dp[i - arr[j]].equals("")) continue;
+                dp[i] = compareString(dp[i], dp[i - arr[j]] + j);
+            }
+        }
         for (int i = 0; i < T; i++) {
             int n = Integer.parseInt(br.readLine());
             String maxResult = calcMax(n);
-            System.out.println(arr[n] + " " + maxResult);
-        }
-//        for (int i = 2; i < 101; i++) {
-//            System.out.println(i + " " + arr[i]);
-//        }
-    }
-
-    public static void initArr() {
-        arr[0] = 0;
-        arr[1] = 0;
-        arr[2] = 1;
-        arr[3] = 7;
-        arr[4] = 4;
-        arr[5] = 2;
-        arr[6] = 0;
-        arr[7] = 8;
-    }
-
-    public static void dp(int n) {
-        int t = 0;
-        for (int i = 2; i <= n/2; i++) {
-            int j = n - i;
-            // 각 수의 첫자리를 비교, 0인 경우는 6개 밖에 없음
-            int a = Integer.parseInt(("" + arr[i]).substring(0, 1));
-            int b = Integer.parseInt((arr[j] + "").substring(0, 1));
-            // 둘 다 0이 아닌 경우
-            if (a != 0 && b != 0) {
-                if (a <= b) {
-                    calc(n, arr[i], arr[j]);
-                } else {
-                    calc(n, arr[j], arr[i]);
-                }
-            } else if (a == 0) {
-                if (6 < b) {
-                    calc(n, 6, arr[j]);
-                } else if (b > 0) {
-                    calc(n, arr[j], 0);
-                }
-            } else if (b == 0) {
-                if (6 < a) {
-                    calc(n, 6, arr[i]);
-                } else if (a > 0) {
-                    calc(n, arr[i], 0);
-                }
-            }
-        }
-    }
-
-    public static void calc(int n, long a, long b) {
-        long result = Long.parseLong(a + "" + b);
-        if (arr[n] == 0) {
-            arr[n] = result;
-        } else {
-            arr[n] = Math.min(arr[n], result);
+            System.out.println(dp[n] + " " + maxResult);
         }
     }
 
