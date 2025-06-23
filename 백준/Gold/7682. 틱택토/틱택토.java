0c77062979614c1b3608798dc955eec5213620d0
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-06-23 21:40:49 +0900

    [Gold V] Title: 틱택토, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -4,96 +4,48 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+//        - X 개수 == O 개수 or X 개수 == O 개수 + 1 이어야 함
+//        - X가 이긴 경우 → X 개수 == O 개수 + 1
+//        - O가 이긴 경우 → X 개수 == O 개수
+//        - 둘 다 동시에 이길 수 없음
+//        - 아무도 안 이기고, 칸이 다 찼다면 valid
+
 class Main {
-    static boolean flag = false;
+    static int[][] winCases = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // 가로
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // 세로
+            {0, 4, 8}, {2, 4, 6}           // 대각선
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            flag = false;
-            String input = br.readLine();
-            if (input.equals("end")) {
-                break;
+        String input;
+        while (!(input = br.readLine()).equals("end")) {
+            char[] board = input.toCharArray();
+            int xCount = 0, oCount = 0;
+            for (char c : board) {
+                if (c == 'X') xCount++;
+                if (c == 'O') oCount++;
             }
-            if (!calc1(input)) {
-                printAnswer(false);
-                continue;
-            }
-            char[] ca = input.toCharArray();
-            boolean[] bX = new boolean[9];
-            boolean[] bO = new boolean[9];
-            char[] bb = new char[9];
-            for (int i = 0; i < 9; i++) {
-                bb[i] = 'a';
-                if (ca[i] == 'X') {
-                    bX[i] = true;
-                } else if (ca[i] == 'O') {
-                    bO[i] = true;
-                }
-            }
-            // X와 O를 번갈아가며 체크 -> 한번이라도 XO 깔끔하게 순서대로 성공 시 flag=true
-            dfs(bb, bX, bO, true);
-            // 전체 채웠지만 빙고 없으면서 .이 없는 경우
-            if (!flag && input.indexOf(".") == -1 && !check(ca)) {
-                flag = true;
-            }
-            printAnswer(flag);
+
+            boolean xWin = isWin(board, 'X');
+            boolean oWin = isWin(board, 'O');
+
+            boolean valid = false;
+            if (xCount == oCount + 1 && xWin && !oWin) valid = true;
+            else if (xCount == oCount && oWin && !xWin) valid = true;
+            else if (!xWin && !oWin && xCount + oCount == 9 && xCount > oCount) valid = true;
+
+            System.out.println(valid ? "valid" : "invalid");
         }
     }
 
-    //    개수로 체크
-    public static boolean calc1(String input) {
-        int lengthX = input.replaceAll("[^X]", "").length();
-        int lengthO = input.replaceAll("[^O]", "").length();
-        if (lengthX >= lengthO && lengthO >= lengthX - 1) {
-            return true;
+    public static boolean isWin(char[] board, char c) {
+        for (int[] line : winCases) {
+            if (board[line[0]] == c && board[line[1]] == c && board[line[2]] == c) {
+                return true;
+            }
         }
         return false;
-    }
-
-    public static void dfs(char[] bb, boolean[] bX, boolean[] bO, boolean isX) {
-        if (flag) return;
-        if (check(bb)) {
-            for (int i = 0; i < 9; i++) {
-                if (bX[i] || bO[i]) return;
-            }
-            flag = true;
-            return;
-        }
-        for (int i = 0; i < 9; i++) {
-            if (isX && bX[i]) {
-                bX[i] = false;
-                bb[i] = 'X';
-                dfs(bb, bX, bO, false);
-                bX[i] = true;
-                bb[i] = 'a';
-            } else if (!isX && bO[i]) {
-                bO[i] = false;
-                bb[i] = 'O';
-                dfs(bb, bX, bO, true);
-                bO[i] = true;
-                bb[i] = 'a';
-            }
-        }
-    }
-
-    public static boolean check(char[] bb) {
-        if (bb[0] != 'a' && bb[0] == bb[1] && bb[1] == bb[2]) return true;
-        if (bb[0] != 'a' && bb[0] == bb[3] && bb[3] == bb[6]) return true;
-        if (bb[0] != 'a' && bb[0] == bb[4] && bb[4] == bb[8]) return true;
-        if (bb[1] != 'a' && bb[1] == bb[4] && bb[4] == bb[7]) return true;
-        if (bb[2] != 'a' && bb[2] == bb[5] && bb[5] == bb[8]) return true;
-        if (bb[2] != 'a' && bb[2] == bb[4] && bb[4] == bb[6]) return true;
-        if (bb[3] != 'a' && bb[3] == bb[4] && bb[4] == bb[5]) return true;
-        if (bb[6] != 'a' && bb[6] == bb[7] && bb[7] == bb[8]) return true;
-        return false;
-    }
-
-    public static void printAnswer(boolean answer) {
-        if (answer) {
-            System.out.println("valid");
-        } else {
-            System.out.println("invalid");
-        }
     }
 }
