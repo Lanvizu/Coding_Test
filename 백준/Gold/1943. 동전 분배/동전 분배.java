8cdb602cdeb8eed68c3ff3df48ceb637d0f7f4bb
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-06-24 16:02:30 +0900

    [Gold II] Title: 동전 분배, Time: 288 ms, Memory: 74920 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -7,12 +7,11 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int stage = 3;
-        while (stage > 0) {
+        for (int i = 0; i < 3; i++) {
             int N = Integer.parseInt(br.readLine());
-            List<int[]> coins = new ArrayList<>();
             int total = 0;
-            for (int i = 0; i < N; i++) {
+            List<int[]> coins = new ArrayList<>();
+            for (int j = 0; j < N; j++) {
                 String[] s = br.readLine().split(" ");
                 int v = Integer.parseInt(s[0]);
                 int c = Integer.parseInt(s[1]);
@@ -21,30 +20,29 @@ class Main {
             }
             if (total % 2 != 0) {
                 System.out.println(0);
-                stage--;
                 continue;
             }
             calc(coins, total / 2);
-            stage--;
         }
     }
 
     public static void calc(List<int[]> coins, int target) {
         boolean[] dp = new boolean[target + 1];
         dp[0] = true;
-
         for (int[] coin : coins) {
             int v = coin[0];
             int c = coin[1];
             int[] used = new int[target + 1];
             Arrays.fill(used, -1);
-            for (int j = 0; j <= target; j++) {
-                if (dp[j]) used[j] = 0;
+
+            for (int i = 0; i <= target; i++) {
+                if (dp[i]) used[i] = 0;
             }
-            for (int j = v; j <= target; j++) {
-                if (!dp[j] && j >= v && dp[j - v] && used[j - v] < c) {
-                    dp[j] = true;
-                    used[j] = used[j - v] + 1;
+
+            for (int i = v; i <= target; i++) {
+                if (!dp[i] && dp[i - v] && used[i - v] < c) {
+                    dp[i] = true;
+                    used[i] = used[i - v] + 1;
                 }
             }
         }
