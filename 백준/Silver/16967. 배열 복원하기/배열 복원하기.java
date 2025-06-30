eb7250b0b0d266a9e0f3a45d96ed5b57084813f6
JunhoLee <121706341+Lanvizu@users.noreply.github.com>
2025-06-30 11:13:08 +0900

    [Silver III] Title: 배열 복원하기, Time: 1016 ms, Memory: 58164 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -7,7 +7,7 @@ import java.io.*;
 class Main {
 
     static int H, W, X, Y;
-    static int[][] A, B;
+    static int[][] B;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,52 +17,27 @@ class Main {
         X = Integer.parseInt(s[2]);
         Y = Integer.parseInt(s[3]);
 
-        A = new int[H][W];
         B = new int[H + X][W + Y];
         for (int i = 0; i < H + X; i++) {
             B[i] = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt).toArray();
         }
-        calc1();
-        calc2();
-        printA();
+        calc();
+        printB();
     }
 
-    public static void calc1() {
-        for (int i = 0; i < X; i++) {
-            for (int j = 0; j < W; j++) {
-                A[i][j] = B[i][j];
-            }
-        }
-        for (int i = H; i < H + X; i++) {
-            for (int j = Y; j < W + Y; j++) {
-                A[i-X][j-Y] = B[i][j];
-            }
-        }
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < Y; j++) {
-                A[i][j] = B[i][j];
-            }
-        }
-        for (int i = X; i < H + X; i++) {
-            for (int j = W; j < W + Y; j++) {
-                A[i-X][j-Y] = B[i][j];
+    public static void calc() {
+        for (int i = X; i < H; i++) {
+            for(int j = Y; j < W; j++) {
+                B[i][j] = B[i][j] - B[i - X][j - Y];
             }
         }
     }
 
-    public static void calc2() {
-        for (int i = X; i < H - X; i++) {
-            for(int j = Y; j < W - Y; j++) {
-                A[i][j] = B[i][j] - A[i - X][j - Y];
-            }
-        }
-    }
-
-    public static void printA() {
+    public static void printB() {
         for(int i = 0; i < H; i++) {
             for(int j = 0; j < W; j++) {
-                System.out.print(A[i][j] + " ");
+                System.out.print(B[i][j] + " ");
             }
             System.out.println();
         }
